63c582cd2b7607bb0890b775a1d3046d88f87741
davidblaa <davidblaa@36c716e5-9d94-432c-9e14-385fd085b95d>
2015-04-03 17:43:26 +0000

    Entities und Mappingfiles angepasst. Relations müssen noch geprüft werden.
    Da brauch ich nen fachman der den Daumen hochhebt

--- Trunk/MCT/Component/DB/MCT.DB.Entities/Species.cs
+++ Trunk/MCT/Component/DB/MCT.DB.Entities/Species.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace MCT.DB.Entities
 {
-    public class Animal:Species
+    public class Species:Node
     {
-
     }
 }
