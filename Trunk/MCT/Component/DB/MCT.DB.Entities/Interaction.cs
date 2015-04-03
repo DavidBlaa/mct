f279761002ec14a08c21e44642983948e08b0ff6
davidblaa <davidblaa@36c716e5-9d94-432c-9e14-385fd085b95d>
2015-04-03 18:40:13 +0000

    Interaction Mapping hinzugefügt

---
+++ Trunk/MCT/Component/DB/MCT.DB.Entities/Interaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCT.DB.Entities
+{
+    public class Interaction
+    {
+        public virtual long Id { get; set; }
+        public virtual Subject Subject { get; set; }
+        public virtual Predicate Predicate { get; set; }
+        public virtual Subject Object { get; set; }
+        public virtual Subject ImpactSubject { get; set; }
+        public virtual int Indicator { get; set; }
+        
+    }
+}
