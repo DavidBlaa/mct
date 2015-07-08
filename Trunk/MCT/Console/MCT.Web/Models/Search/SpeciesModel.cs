a03e7ae84fc09dc642cf6e966fe33e3b4e3b4f6a
davidblaa <davidblaa@36c716e5-9d94-432c-9e14-385fd085b95d>
2015-07-08 05:19:51 +0000

    add missing species view and Models/Search/SpeciesModel.cs :/

---
+++ Trunk/MCT/Console/MCT.Web/Models/Search/SpeciesModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using MCT.DB.Entities;
+
+namespace MCT.Web.Models.Search
+{
+    public class SpeciesModel
+    {
+        public long Id { get; set; }
+        public String Name { get; set; }
+        public String ScientificName { get; set; }
+        public String Description { get; set; }
+        public String ImagePath { get; set; }
+
+        public SpeciesType Type { get; set; }
+
+        public static SpeciesModel Convert(Species species)
+        {
+
+            SpeciesModel model = new SpeciesModel();
+
+            //SubjectLevel
+
+            model.Id = species.Id;
+
+            if (!String.IsNullOrEmpty(species.Name))
+                model.Name = species.Name;
+
+            if (!String.IsNullOrEmpty(species.Description))
+                model.Description = species.Description;
+
+            model.Type = GetType(species);
+
+            if (!species.Medias.Any())
+            {
+                model.ImagePath = "/Images/Empty.png";
+            }
+            else
+            {
+                model.ImagePath =  species.Medias.First().ImagePath;
+            }
+
+            // Species Level
+            model.ScientificName = species.ScientificName;
+
+
+            return model;
+        }
+
+        protected static SpeciesType GetType(Species species)
+        {
+            if (species is Animal) return SpeciesType.Animal;
+            if(species is Plant) return SpeciesType.Plant;
+
+            return SpeciesType.Unknow;
+
+        }
+    }
+
+    public enum SpeciesType
+    { 
+        Animal,
+        Plant,
+        Unknow
+    }
+}
