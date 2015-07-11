57bbb4cc457ad26a02b5e36ff18ad495d993b356
davidblaa <davidblaa@36c716e5-9d94-432c-9e14-385fd085b95d>
2015-07-11 12:12:51 +0000

    erste test umsetzung erzeugen von einem lucene index auf subjekte sucheabfragen auf Name greifen auf den index zu und liefern ergebnisse.
    nächste schritte sind konzeptionelle planung der suche

--- Trunk/MCT/Modules/MCT.Helpers/NHibernateHelper.cs
+++ Trunk/MCT/Modules/MCT.Helpers/NHibernateHelper.cs
@@ -4,6 +4,11 @@ using NHibernate;
 using NHibernate.Cfg;
 using NHibernate.Search;
 using NHibernate.Search.Impl;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using System.Diagnostics;
 
 namespace MCT.Helpers
 {
@@ -60,18 +65,56 @@ namespace MCT.Helpers
         {
             IFullTextSession session = new FullTextSessionImpl(GetCurrentSession());
             ITransaction tx = session.BeginTransaction();
-            session.PurgeAll(typeof(Species));
+            session.PurgeAll(typeof(Subject));
             tx.Commit();
 
             tx = session.BeginTransaction();
 
-            foreach (object entity in session.CreateCriteria(typeof(Species)).List())
+            
+
+            foreach (object entity in session.CreateCriteria(typeof(Subject)).List())
             {
                 session.Index(entity);
             }
 
             tx.Commit();
             session.Close();
+
+        }
+
+        public static void Search()
+        {
+                HttpContext context = HttpContext.Current;
+                ISession currentSession = context.Items[CurrentSessionKey] as ISession;
+
+                if (currentSession == null)
+                {
+                    currentSession = sessionFactory.OpenSession();
+                    context.Items[CurrentSessionKey] = currentSession;
+                }
+
+                IFullTextSession session = new FullTextSessionImpl(currentSession);
+                if (!currentSession.IsOpen)
+                {
+                    currentSession = sessionFactory.OpenSession();
+                    context.Items[CurrentSessionKey] = currentSession;
+
+                    if (currentSession.IsOpen)
+                        Debug.WriteLine("Session open");
+                    else
+                        Debug.WriteLine("Session Closed");
+                }
+
+                string searchQuery = "Name:2";
+                Analyzer std = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_29);
+
+                QueryParser parser = new QueryParser(Lucene.Net.Util.Version.LUCENE_30, "Name", std);
+                Query query = parser.Parse(searchQuery);
+
+                IFullTextSession fullTextSession = NHibernate.Search.Search.CreateFullTextSession(NHibernateHelper.GetCurrentSession());
+                IFullTextQuery fullTextQuery = fullTextSession.CreateFullTextQuery(query, typeof(Species));
+
+                var employeeList = fullTextQuery.List();
 
         }
     }
