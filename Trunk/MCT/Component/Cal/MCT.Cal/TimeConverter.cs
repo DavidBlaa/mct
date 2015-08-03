995ed1b0a47ca64769beb39f2a17b0dad9de4727
davidblaa <davidblaa@36c716e5-9d94-432c-9e14-385fd085b95d>
2015-08-03 09:47:14 +0000

    calendar hinzugefügt calendar zeit alle plants mit sowing, harvest, seedMaturity, bloom date in einer nicht komfortablen monatssicht

--- Trunk/MCT/Component/Cal/MCT.Cal/TimeConverter.cs
+++ Trunk/MCT/Component/Cal/MCT.Cal/TimeConverter.cs
@@ -1,9 +1,19 @@
-﻿namespace MCT.Cal
+﻿using System;
+
+namespace MCT.Cal
 {
     public class TimeConverter
     {
+        public static DateTime GetStartDateTime(int monthIndex)
+        {
+            DateTime temp = new DateTime(DateTime.Today.Year,monthIndex+1,1);
+            return temp;
+        }
 
+        public static DateTime GetEndDateTime(int monthIndex)
+        {
+            DateTime temp = new DateTime(DateTime.Today.Year, monthIndex + 1, DateTime.DaysInMonth(DateTime.Today.Year,monthIndex + 1));
+            return temp;
+        }
     }
-
-    
 }
---
+++ Trunk/MCT/Console/MCT.Web/Controllers/CalendarController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MCT.Cal;
+using MCT.DB.Entities;
+using MCT.DB.Services;
+
+namespace MCT.Web.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            SubjectManager subjectmanager = new SubjectManager();
+
+            var events=new List<object>();
+
+            foreach (var VARIABLE in subjectmanager.GetAll<TimePeriod>())
+            {
+                events.Add(getEventFromTimeperiod(VARIABLE, VARIABLE.Type));
+            }
+
+            return Json(events.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+
+        private object getEventFromTimeperiod(TimePeriod tp, TimePeriodType type)
+        {
+            string color = "";
+
+            switch (type)
+            {
+                case TimePeriodType.Sowing: {color = "green";break;}
+                case TimePeriodType.Harvest:{color = "blue";break;}
+                case TimePeriodType.Bloom:{color = "yellow";break;}
+                case TimePeriodType.SeedMaturity:{color = "red";break;}
+            }
+
+            return
+                new
+                {
+                    title = tp.AssignedTo.Name,
+                    start = TimeConverter.GetStartDateTime((int) tp.StartMonth).ToString("yyyy-MM-dd", CultureInfo.CreateSpecificCulture("en-US")),
+                    end = TimeConverter.GetEndDateTime((int) tp.EndMonth).ToString("yyyy-MM-dd", CultureInfo.CreateSpecificCulture("en-US")),
+                    backgroundColor = color
+                };
+        }
+    }
+}
