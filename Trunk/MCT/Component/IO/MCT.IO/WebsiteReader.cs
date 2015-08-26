3ed7a30e1d97befb30760013f1236588ea83b9ab
davidblaa <davidblaa@36c716e5-9d94-432c-9e14-385fd085b95d>
2015-08-26 08:25:47 +0000

    start mit auslesen von wikipedia im testcontroller

---
+++ Trunk/MCT/Component/IO/MCT.IO/WebsiteReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCT.IO
+{
+    public class WebsiteReader
+    {
+        public string BaseUrl { get; set; }
+        public string Html { get; set; }
+
+        public WebsiteReader()
+        {
+
+        }
+
+    }
+}
---
+++ Trunk/MCT/Component/IO/MCT.IO/WikipediaReader.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCT.IO
+{
+    public class WikipediaReader:WebsiteReader
+    {
+        public WikipediaReader()
+        {
+            BaseUrl = @"https://de.wikipedia.org/wiki";
+        }
+
+        private void LoadSubjectPage(string name)
+        {
+            string urlAddress = Path.Combine(BaseUrl,name);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                this.Html = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+        }
+
+        public string GetScientificName(string name)
+        {
+            string scientificName = "";
+
+            if (String.IsNullOrEmpty(this.Html))
+                LoadSubjectPage(name);
+
+            int namePosition = this.Html.IndexOf("<p><b>" + name + "</b>");
+
+            if (namePosition != -1)
+            {
+                string body = this.Html.Substring(namePosition);
+
+                int start = body.IndexOf('(');
+                int end = body.IndexOf(')');
+
+                if (start != -1 && end != -1)
+                {
+                    scientificName = body.Substring(start, end);
+                }
+            }
+
+            return scientificName;
+        }
+    }
+}
