de726e962df94da802ed1876605273c8947d4ace
David Blaa <DavidBlaa@users.noreply.github.com>
2017-05-03 19:48:31 +0000

    add missing files

---
+++ Console/MCT.Web/Models/AnimalModel.cs
@@ -0,0 +1,52 @@
+﻿using MCT.DB.Entities;
+using System;
+using System.Linq;
+
+namespace MCT.Web.Models
+{
+    public class AnimalModel : SubjectModel
+    {
+        public string ScientificName { get; set; }
+        public string TaxonRank { get; set; }
+
+        public static AnimalModel Convert(Animal animal)
+        {
+            AnimalModel model = new AnimalModel();
+
+            model.Id = animal.Id;
+
+            #region subject
+            if (!String.IsNullOrEmpty(animal.Name))
+                model.Name = animal.Name;
+
+            if (!String.IsNullOrEmpty(animal.Rank.ToString()))
+                model.TaxonRank = animal.Rank.ToString();
+
+
+            if (!String.IsNullOrEmpty(animal.Description))
+                model.Description = animal.Description;
+
+            model.Type = GetType(animal);
+
+            if (animal.Medias.Count() == 0)
+            {
+                model.ImagePath = "/Images/Empty.png";
+            }
+            else
+            {
+                model.ImagePath = animal.Medias.First().ImagePath;
+            }
+            #endregion
+
+            model.ScientificName = animal.ScientificName;
+
+            #region loadParentModels
+
+            model.Parent = SimpleNodeViewModel.Convert(animal.Parent);
+
+            #endregion
+
+            return model;
+        }
+    }
+}
---
+++ Console/MCT.Web/Models/SimpleNodeViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MCT.DB.Entities;
+using System;
+
+namespace MCT.Web.Models
+{
+    public class SimpleNodeViewModel
+    {
+        public String Name { get; set; }
+        public String ScientificName { get; set; }
+        public String Description { get; set; }
+        public string TaxonRank { get; set; }
+        public SimpleNodeViewModel Parent { get; set; }
+
+        public static SimpleNodeViewModel Convert(Node node)
+        {
+            SimpleNodeViewModel model = new SimpleNodeViewModel();
+
+            model.Name = node.Name;
+            model.ScientificName = node.ScientificName;
+            model.Description = node.Description;
+            model.TaxonRank = node.Rank.ToString();
+
+            if (node.Parent != null)
+            {
+                model.Parent = Convert(node.Parent);
+
+            }
+
+            return model;
+        }
+    }
+}
