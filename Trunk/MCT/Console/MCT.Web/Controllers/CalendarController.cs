19a0b88f825768af8c26b1ceec8374a0b832f3a8
davidblaa <davidblaa@36c716e5-9d94-432c-9e14-385fd085b95d>
2017-07-06 14:11:46 +0000

    first try to add gantt chart

--- Trunk/MCT/Console/MCT.Web/Controllers/CalendarController.cs
+++ Trunk/MCT/Console/MCT.Web/Controllers/CalendarController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using MCT.Cal;
+﻿using MCT.Cal;
 using MCT.DB.Entities;
 using MCT.DB.Services;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Web.Mvc;
 
 namespace MCT.Web.Controllers
 {
@@ -18,15 +16,18 @@ namespace MCT.Web.Controllers
             return View();
         }
 
+        #region calendar
+
         public JsonResult GetEvents()
         {
             SubjectManager subjectmanager = new SubjectManager();
 
-            var events=new List<object>();
+            var events = new List<object>();
 
             foreach (var VARIABLE in subjectmanager.GetAll<TimePeriod>())
             {
-                events.Add(getEventFromTimeperiod(VARIABLE, VARIABLE.Type));
+                if (VARIABLE != null)
+                    events.Add(getEventFromTimeperiod(VARIABLE, VARIABLE.Type));
             }
 
             return Json(events.ToArray(), JsonRequestBehavior.AllowGet);
@@ -36,22 +37,84 @@ namespace MCT.Web.Controllers
         {
             string color = "";
 
+            Debug.WriteLine(tp);
+
             switch (type)
             {
-                case TimePeriodType.Sowing: {color = "green";break;}
-                case TimePeriodType.Harvest:{color = "blue";break;}
-                case TimePeriodType.Bloom:{color = "yellow";break;}
-                case TimePeriodType.SeedMaturity:{color = "red";break;}
+                case TimePeriodType.Sowing: { color = "green"; break; }
+                case TimePeriodType.Harvest: { color = "blue"; break; }
+                case TimePeriodType.Bloom: { color = "yellow"; break; }
+                case TimePeriodType.SeedMaturity: { color = "red"; break; }
             }
 
-            return
-                new
-                {
-                    title = tp.AssignedTo.Name,
-                    start = TimeConverter.GetStartDateTime((int) tp.StartMonth).ToString("yyyy-MM-dd", CultureInfo.CreateSpecificCulture("en-US")),
-                    end = TimeConverter.GetEndDateTime((int) tp.EndMonth).ToString("yyyy-MM-dd", CultureInfo.CreateSpecificCulture("en-US")),
-                    backgroundColor = color
-                };
+            var json = new
+            {
+                title = tp.AssignedTo.Name,
+                start = TimeConverter.GetStartDateTime((int)tp.StartMonth).ToString("yyyy-MM-dd", CultureInfo.CreateSpecificCulture("en-US")),
+                end = TimeConverter.GetEndDateTime((int)tp.EndMonth).ToString("yyyy-MM-dd", CultureInfo.CreateSpecificCulture("en-US")),
+                backgroundColor = color
+            };
+
+            Debug.WriteLine(json);
+
+            if (json != null)
+                return json;
+
+            return null;
         }
+
+        #endregion calendar
+
+        #region Gantt
+
+        public JsonResult GetEventsForGantt()
+        {
+            SubjectManager subjectmanager = new SubjectManager();
+
+            var events = new List<object>();
+
+            foreach (var VARIABLE in subjectmanager.GetAll<TimePeriod>())
+            {
+                if (VARIABLE != null)
+                    events.Add(getEventFromTimeperiodForGantt(VARIABLE, VARIABLE.Type));
+            }
+
+            return Json(events.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+
+        private object getEventFromTimeperiodForGantt(TimePeriod tp, TimePeriodType type)
+        {
+            string color = "";
+
+            Debug.WriteLine(tp);
+
+            switch (type)
+            {
+                case TimePeriodType.Sowing: { color = "green"; break; }
+                case TimePeriodType.Harvest: { color = "blue"; break; }
+                case TimePeriodType.Bloom: { color = "yellow"; break; }
+                case TimePeriodType.SeedMaturity: { color = "red"; break; }
+            }
+
+            var fromDT = TimeConverter.GetStartDateTime((int)tp.StartMonth).ToShortDateString();
+            var ToDT = TimeConverter.GetStartDateTime((int)tp.StartMonth).ToShortDateString();
+
+            var json = new
+            {
+                label = tp.AssignedTo.Name,
+                from = "/Date(," + fromDT + "/",
+                to = "/Date(," + ToDT + "/"
+
+            };
+
+            Debug.WriteLine(json);
+
+            if (json != null)
+                return json;
+
+            return null;
+        }
+
+        #endregion Gantt
     }
 }
