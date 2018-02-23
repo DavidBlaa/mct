4b29a1ca75cc75c62f5f82f2a4d90a01eb4d9dd7
David Blaa <dblaa@bgc-jena.mpg.de>
2018-02-23 16:03:32 +0100

    - add placement and patch to the system

---
+++ Component/DB/MCT.DB.Entities/PatchPlaner/Patch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+
+namespace MCT.DB.Entities.PatchPlaner
+{
+
+    public class Patch
+    {
+        public virtual long Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public virtual LocationType LocationType { get; set; }
+        public virtual NutrientClaim NutrientClaim { get; set; }
+
+        public virtual ICollection<Placement> Placements { get; set; }
+    }
+
+}
---
+++ Component/DB/MCT.DB.Entities/PatchPlaner/Placement.cs
@@ -0,0 +1,13 @@
+﻿namespace MCT.DB.Entities.PatchPlaner
+{
+
+    public class Placement
+    {
+        public virtual long Id { get; set; }
+        public virtual Plant Plant { get; set; }
+        public virtual string Transformation { get; set; }
+        public virtual TimePeriodArea PlantingArea { get; set; }
+        public virtual TimePeriodMonth PlantingMonth { get; set; }
+        public virtual Patch Patch { get; set; }
+    }
+}
