221b8fbde22fbb4f66b2957d98bba06f83660ba2
David Schöne <david.blaa@googlemail.com>
2021-07-12 08:40:15 +0200

    add new api for statistic

---
+++ Console/MCT.Web/Controllers/API/StatisticController.cs
@@ -0,0 +1,43 @@
+﻿using MCT.Web.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace MCT.Web.Controllers.API
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class StatisticController : ApiController
+    {
+        // GET api/<controller>
+        [Route("api/statistic")]
+        public Dictionary<string,int> Get()
+        {
+            return ModelHelper.GetStatistics();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
